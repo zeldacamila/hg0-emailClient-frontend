b519e9443d7b8cbc21bec5a0f81b8ddebc748bba
Maria Fernanda Palencia <39041288+mafer23@users.noreply.github.com>
2024-02-24 13:36:39 -0500

    Login
    Login , ya pude solucionar lo de github

--- email-app/src/App.tsx
+++ email-app/src/App.tsx
@@ -2,7 +2,7 @@ import { Navigate, Route, Routes } from 'react-router-dom';
 import './App.css';
 import NotFound from './pages/NotFound';
 import SignUp from './pages/SignUp/SignUp';
-import LogIn from './pages/LogIn';
+import LogIn from './pages/Login/LogIn';
 
 function App() {
   return (
---
+++ email-app/src/pages/Login/LogIn.tsx
@@ -0,0 +1,15 @@
+import Login from '../../components/Login/Login';
+
+import './Login.css';
+
+
+
+const LogIn: React.FC = () => {
+  return (
+    <div className="login-container">
+      <Login /> 
+    </div>
+  );
+};
+
+export default LogIn; 
