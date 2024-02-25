ab434d5c9abda2d670ec2080dc98fce5f29b6a77
Maria <zeldacamila0913@gmail.com>
2024-02-25 12:03:56 -0500

    shows username on mail page header

--- email-app/src/components/Header/index.tsx
+++ email-app/src/components/Header/index.tsx
@@ -1,44 +1,53 @@
 import { LogoutOutlined, UserOutlined } from '@ant-design/icons';
-import { Menu, Layout, Dropdown, Avatar  } from 'antd';
+import { Menu, Layout, Dropdown, Avatar } from 'antd';
 import { FC } from 'react';
-import { useAppDispatch } from '../../hooks';
+import { useAppDispatch, useAppSelector } from '../../hooks';
 import { removeUser } from '../../features/auth/userSlice';
 
 const { Header } = Layout;
 
 const HeaderComponent: FC = () => {
-    const dispatch = useAppDispatch();
-    return (
-    <Header
-     style={{ display: 'flex', alignItems: 'flex-end' }}>
-        <Menu
-          theme="dark"
-          mode="horizontal"
-          selectable={false}
-          items={[
-            { key: 1, label: (
-                <Dropdown menu={{ 
-                    items: [
-                        { 
-                            key: 2, 
-                            label: 'Logout',
-                            icon: <LogoutOutlined />,
-                            onClick: () => dispatch(removeUser()),
-                        }
-                    ]
-                    }} 
-                    placement="bottomLeft"
-                 >
-                    <Avatar style={{ backgroundColor: '#87d068' }} icon={<UserOutlined />} />
-                </Dropdown>
+  const dispatch = useAppDispatch();
+  const user = useAppSelector((state) => state.user.value)?.username;
+  return (
+    <Header style={{ display: 'flex', alignItems: 'flex-end' }}>
+      <Menu
+        theme="dark"
+        mode="horizontal"
+        selectable={false}
+        items={[
+          {
+            key: 2,
+            label: <span>Welcome {user || ''} </span>,
+          },
+          {
+            key: 1,
+            label: (
+              <Dropdown
+                menu={{
+                  items: [
+                    {
+                      key: 2,
+                      label: 'Logout',
+                      icon: <LogoutOutlined />,
+                      onClick: () => dispatch(removeUser()),
+                    },
+                  ],
+                }}
+                placement="bottomLeft"
+              >
+                <Avatar
+                  style={{ backgroundColor: '#87d068' }}
+                  icon={<UserOutlined />}
+                />
+              </Dropdown>
             ),
-        
-        },
-          ]}
-          style={{ flex: 1, justifyContent: 'flex-end'}}
-        />
-      </Header>
-    );
+          },
+        ]}
+        style={{ flex: 1, justifyContent: 'flex-end' }}
+      />
+    </Header>
+  );
 };
 
 export default HeaderComponent;
