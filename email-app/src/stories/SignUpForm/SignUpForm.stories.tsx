c700698c71625cc42d7306dc9f38527b3c6d3915
Julian Rodriguez <est.julianan.rodri2@unimilitar.edu.co>
2024-03-23 20:02:25 -0500

    fix: delete line to conflict

--- email-app/src/stories/SignUpForm/SignUpForm.stories.tsx
+++ email-app/src/stories/SignUpForm/SignUpForm.stories.tsx
@@ -3,7 +3,6 @@ import { StoryFn, Meta } from '@storybook/react';
 import { Provider } from 'react-redux';
 import { store } from '../../store'; // Assuming your store is accessible
 import { BrowserRouter } from 'react-router-dom';
-import React from 'react';
 
 export default {
   title: 'Componentes/SignUp/SignUpForm',
